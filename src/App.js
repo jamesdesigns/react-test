27b47f9a80b466c4027c37cc47501f4de81e3069
James Hooper <jameshooperdesigns@gmail.com>
2019-09-16 18:03:54 -0600

    Add a switchNameHandler for a click event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'James', age: 34},
+      { name: 'Linda', age: 24},
+      { name: 'JJ', age: 2}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // Don't DO THIS ---- this.state.persons[0].name = 'Jimmy';
+    this.setState( {
+      persons: [
+        { name: 'Jimmy', age: 34 },
+        { name: 'Linda', age: 24 },
+        { name: 'JJ', age: 1 }
+      ]
+    } )
+  }
+
+// function App() {
+  render() {
   return (
     <div className="App">
       <h1>This is a React Test Site</h1>
+      <p>This is really working!</p>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+  
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} >My Hobbies: Drawing</Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
     </div>
   );
+  // return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work now?'));
+}
 }
 
 export default App;
+
