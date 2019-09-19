1fa80f20497513b38d4b68a4d1a46a7e3fd2b66a
James Hooper <jameshooperdesigns@gmail.com>
2019-09-19 17:31:57 -0600

    Add new h1 title New Players on line 7

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ import './Person.css';
 const person = (props) => {
     return (
         <div className="Person">
+            <h1>New Players</h1>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
