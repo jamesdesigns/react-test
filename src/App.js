30000c244ece4e07095a2271a251d00fea81b8c9
James Hooper <jameshooperdesigns@gmail.com>
2019-09-19 16:24:04 -0600

    Improve the nameChangedHandler on line 18 in the App.js

--- src/App.js
+++ src/App.js
@@ -15,17 +15,23 @@ class App extends Component {
     showPersons: false
   };
 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
 
+    const person = {
+      ...this.state.persons[personIndex]
+    };
 
+    // Alternative to the following above
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: event.target.value, age: 34 },
-        { name: 'Linda', age: 24 },
-        { name: 'JJ', age: 2}
-      ]
-    })
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -62,7 +68,8 @@ render () {
             click={() => this.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
-            key={person.id} />
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
         })
         }
     </div> 
